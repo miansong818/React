a586b1025a1ffe2cb51657f016cc70a9beef0269
Mian Song <miansong818@gmail.com>
2022-07-07 01:20:27 -0400

    done

--- first_app/src/App.js
+++ first_app/src/App.js
@@ -1,14 +1,23 @@
+/* eslint-disable max-len */
+/* eslint-disable no-unused-vars */
 // import './App.css';
-import {useState, useEffect} from 'react';
-import SearchIcon from './search.svg';
-import MovieCard from './MovieCard';
-import Posts from './Posts';
-import DataTable from './DataTable';
+import React, {useState, useEffect} from 'react';
+// import SearchIcon from './search.svg';
+// import MovieCard from './MovieCard';
+// import Posts from './Posts';
+// import DataTable from './DataTable';
 import Hooks from './Hooks';
-
-  //API key: 54fff2a7
-  //url: http://www.omdbapi.com/?i=tt3896198&apikey=54fff2a7
-const API_URL='http://www.omdbapi.com/?apikey=54fff2a7'; asdsad
+import Home from './components/home';
+import Dashboard from './components/dashboard';
+import {BrowserRouter, Routes, Route, Outlet, Link} from 'react-router-dom';
+import ResizeWindow from './resizeWindown';
+import DataTable from './DataTable';
+import JokePad from './components/jokePad';
+import Posts from './Posts';
+import 'bootstrap/dist/css/bootstrap.css';
+// API key: 54fff2a7
+// url: http://www.omdbapi.com/?i=tt3896198&apikey=54fff2a7
+const API_URL='http://www.omdbapi.com/?apikey=54fff2a7';
 
 // const movies =[{
 //   Poster: "https://m.media-amazon.com/images/M/MV5BYjFhN2RjZTctMzA2Ni00NzE2LWJmYjMtNDAyYTllOTkyMmY3XkEyXkFqcGdeQXVyNTA0OTU0OTQ@._V1_SX300.jpg",
@@ -18,56 +27,85 @@ const API_URL='http://www.omdbapi.com/?apikey=54fff2a7'; asdsad
 //   imdbID: "tt2705436",
 //   }];
 
-const App = ()=> {  
+const App = ()=> {
   const [movies, setMovies] = useState([{}]);
   const [searchTerm, setSearchTerm] = useState('');
+  // document.getElementsByTagName('body')
+  const searchMovies = async (title)=>{
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    // console.log(data);
+    setMovies(data.Search);
+    // return data;
+  };
+  // it's happening often
+  useEffect(()=>{
+    searchMovies('Spiderman');
+  }, []);
 
-  const searchMovies = async(title)=>{
-  const response = await fetch(`${API_URL}&s=${title}`);
-  const data = await response.json();
-  // console.log(data);
-  setMovies(data.Search);
-  // return data;
-  
-};
-  //it's happening often
-  useEffect(()=>{searchMovies('Spiderman')}, []);
 
   return (
-    //JSX movie land
-    // <div className="App">
-    //   <h1>Movie Land</h1>
-    //   <div className='search'>
-    //     <input placeholder='Search for moives' value={searchTerm} onChange={(e)=>setSearchTerm(e.target.value)}/>
-    //     <img src={SearchIcon} alt="search" onClick={()=>searchMovies(searchTerm)} />
-    //   </div>
-    //   {
-    //     movies?.length > 0 ? ( <div className='container'>
-    //       {movies.map((movie)=>(
-    //         <MovieCard movie={movie}/>
-    //       ))}
-    //   </div>) : ( <div className='container'>
-    //     <h2> No movie found</h2>
-    //   </div>)
-    //   }
-     
-    // </div>
+  // JSX movie land
+  // <div className="App">
+  //   <h1>Movie Land</h1>
+  //   <div className='search'>
+  //     <input placeholder='Search for moives' value={searchTerm} onChange={(e)=>setSearchTerm(e.target.value)}/>
+  //     <img src={SearchIcon} alt="search" onClick={()=>searchMovies(searchTerm)} />
+  //   </div>
+  //   {
+  //     movies?.length > 0 ? ( <div className='container'>
+  //       {movies.map((movie)=>(
+  //         <MovieCard movie={movie}/>
+  //       ))}
+  //   </div>) : ( <div className='container'>
+  //     <h2> No movie found</h2>
+  //   </div>)
+  //   }
 
-    //JSX Posts table
-    // <div className='App'>
-    //   <Posts></Posts>
-    // </div>
+  // </div>
 
-      //JSX Posts table
-    // <div className='App'>
-    //   <DataTable />
-    // </div>
-    //Jsx Hooks
-    
+  // JSX Posts table
+  // <div className='App'>
+  //   <Posts></Posts>
+  // </div>
+
+  // JSX Posts table
+  // <div className='App'>
+  //   <DataTable />
+  // </div>
+  // Jsx Hooks
+
+
+  //   <html lang="en">
+  //   <head>
+  //     <meta charset="utf-8">
+  //     <meta name="viewport" content="width=device-width, initial-scale=1">
+  //     <title>Bootstrap demo</title>
+  //     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+  //   </head>
+  //   <body>
+  //     <h1>Hello, world!</h1>
+  //     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+  //   </body>
+  // </html>
+
     <div>
-      <Hooks />
+      <BrowserRouter>
+        <Routes>
+          <Route path='' element={<Hooks />} />
+          <Route path='home' element={<Home />} />
+          <Route path='dataTable' element={<DataTable />} />
+          <Route path='resize' element={<ResizeWindow />} />
+          <Route path='jokes' element={<JokePad />} />
+          <Route path='posts' element={<Posts />} />
+
+        </Routes>
+      </BrowserRouter>
+      {/* <Hooks /> */}
+
+      {/* <DataTable /> */}
     </div>
   );
-}
+};
 
 export default App;
--- first_app/src/family.js
+++ first_app/src/family.js
@@ -3,35 +3,35 @@ import {useState, useEffect} from 'react';
 
 
 const Henry = (props) => {
- return (
-   <>
-   <h1>age: {props.age} </h1>
-   <h1>gender: {props.gender}</h1>
-   <h1>{ props.cute ? 'is cute' : 'not cute at all'}</h1>
-   </>
- );
+  return (
+    <>
+      <h1>age: {props.age} </h1>
+      <h1>gender: {props.gender}</h1>
+      <h1>{ props.cute ? 'is cute' : 'not cute at all'}</h1>
+    </>
+  );
 };
 
 const Cathy = (props) => {
   return (
     <>
-    <h1>age: {props.age} </h1>
-    <h1>gender: {props.gender}</h1>
-    <h1>{ props.cute ? 'is cute' : 'not cute at all'}</h1>
+      <h1>age: {props.age} </h1>
+      <h1>gender: {props.gender}</h1>
+      <h1>{ props.cute ? 'is cute' : 'not cute at all'}</h1>
     </>
   );
- };
+};
 
- 
+
 const App = ()=> {
   const son = {
-    name:  "Henry",
+    name: 'Henry',
     age: 5,
     gender: 'M',
     cute: false,
   };
   const daughter = {
-    name:  "Cathy",
+    name: 'Cathy',
     age: '2 Months',
     gender: 'W',
     cute: true,
@@ -42,38 +42,38 @@ const App = ()=> {
 
   const [counter, setCounter] = useState(son.age);
 
-  //it's happening often
+  // it's happening often
   useEffect(()=>{
-   alert('ohhhh Henry is '+counter +' years old');
- }, [counter]);
+    alert('ohhhh Henry is '+counter +' years old');
+  }, [counter]);
 
   return (
-    //JSX
+    // JSX
     <div className="App">
       <header className="App-header">
-      <h1> Welcome to my home: </h1>
+        <h1> Welcome to my home: </h1>
       </header>
       <body>
-      <div> 
-      <h1>Hi there my son {isHe ?  son.name : daughter.name}</h1>
-      <Henry age={son.age} gender={son.gender} cute={son.cute} />
-      <h1>Henry wil be {counter} yaers old </h1>
-      <div>
-        <button onClick={()=>setCounter((counter)=>counter-1)}>Last year</button>
-      </div>
-      <div>
-        <button onClick={()=>setCounter((counter)=>counter+1)}>Next year</button>
-      </div>
+        <div>
+          <h1>Hi there my son {isHe ? son.name : daughter.name}</h1>
+          <Henry age={son.age} gender={son.gender} cute={son.cute} />
+          <h1>Henry wil be {counter} yaers old </h1>
+          <div>
+            <button onClick={()=>setCounter((counter)=>counter-1)}>Last year</button>
+          </div>
+          <div>
+            <button onClick={()=>setCounter((counter)=>counter+1)}>Next year</button>
+          </div>
 
-      </div> 
-      <div>
-      <h1>Hi there my daughter {!isShe ? (<> {daughter.name} </>)  : (<> {daughter.name} </>)}</h1>
-      
-      <Cathy age={daughter.age} gender={daughter.gender} cute={daughter.cute} />
-      </div>  
+        </div>
+        <div>
+          <h1>Hi there my daughter {!isShe ? (<> {daughter.name} </>) : (<> {daughter.name} </>)}</h1>
+
+          <Cathy age={daughter.age} gender={daughter.gender} cute={daughter.cute} />
+        </div>
       </body>
     </div>
   );
-}
+};
 
 export default App;
